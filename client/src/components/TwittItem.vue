f6481fb2f3133bf1da1edd7839b22691b0bb9784
Kelhen <Kelhen@users.noreply.github.com>
2018-04-12 17:08:58 +0900

    add real user picture
    switch from default image to real user picture in twitt display

--- client/src/components/TwittItem.vue
+++ client/src/components/TwittItem.vue
@@ -1,39 +1,49 @@
 <template>
-  <div class="row border border-light rounded twitt">
-    <div class="col-1" style="padding: 5px">
-      <img src="../assets/default_icon.png" alt="Avatar" width="48" height="48" class="rounded-circle">
-    </div>
-    <div class="col-sm-11">
-      <h5>{{twitt.user.name}}</h5> {{ twitt.created_at | moment("calendar") }}
-    </div>
-      <div class="col-sm-12">
-      <p class="lead">{{twitt.twitt}}</p>
-    </div>
-      <div class="col-12">
-        <font-awesome-icon icon="comment" /> x reply
-      </div>
+<div class="row border border-light rounded twitt">
+  <div class="col-1" style="padding: 5px">
+    <!-- <avatar v-bind:username='usename'></avatar> -->
+    <avatar :username="`${twitt.postedBy.name}`" :src="require(`@/assets/${img}`)" :size="48"></avatar>
   </div>
+  <div class="col-sm-11">
+    <h5>{{twitt.postedBy.name}}</h5> {{ twitt.created_at | moment("calendar") }}
+  </div>
+  <div class="col-sm-12">
+    <p class="lead">{{twitt.twitt}}</p>
+  </div>
+  <div class="col-12">
+    <font-awesome-icon icon="comment" /> x reply
+  </div>
+</div>
 </template>
 
 <script>
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import Avatar from 'vue-avatar'
 
 export default {
   name: 'TwittItem',
-  props: ['twitt'],
+  props: [ 'twitt' ],
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    Avatar
+  },
+  computed: {
+    img () {
+      return this.twitt.postedBy.image
+    },
+    usename () {
+      return this.twitt.postedBy.name
+    }
   }
 }
 </script>
 
 <style lang="scss">
-.twitt
-{
-  margin: 2px;
-  padding-top: 5px;
+.twitt {
+    margin: 2px;
+    padding-top: 5px;
 }
-.twitt:hover{
-  background-color: rgb(230, 236, 240)
+.twitt:hover {
+    background-color: rgb(230, 236, 240);
 }
 </style>
